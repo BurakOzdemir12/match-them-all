00e20710ee0d68a22288e34ba91bdf5e737d8c34
BurakOzdemir <qburak.ozdemir.0640@gmail.com>
2026-03-03 13:48:57 +0300

    feat: Game Manager to manage game state

---
+++ Assets/_Project/Scripts/Managers/GameManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using _Project.Scripts.Enums;
+using _Project.Scripts.Static;
+using UnityEngine;
+
+namespace _Project.Scripts.Managers
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager Instance { get; private set; }
+        public GameState currentGameState { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this) Destroy(this.gameObject);
+
+            Instance = this;
+
+            currentGameState = GameState.Playing;
+        }
+
+        private void OnEnable()
+        {
+            GameEvents.OnGameStarted += HandleGameStarted;
+            GameEvents.OnGamePaused += HandleGamePaused;
+            GameEvents.OnLevelCompleted += HandleLevelCompleted;
+            GameEvents.OnLevelFailed += HandleGameFailed;
+        }
+
+        private void HandleGameFailed()
+        {
+            currentGameState = GameState.LevelFailed;
+            Time.timeScale = 0f;
+        }
+
+        private void HandleLevelCompleted()
+        {
+            currentGameState = GameState.LevelCompleted;
+            Time.timeScale = 1f;
+        }
+
+        private void HandleGamePaused()
+        {
+            currentGameState = GameState.Paused;
+            Time.timeScale = 0f;
+        }
+
+        private void HandleGameStarted()
+        {
+            currentGameState = GameState.Playing;
+            Time.timeScale = 1f;
+        }
+
+        private void TurnToLobby()
+        {
+        }
+
+        private void OnDisable()
+        {
+            GameEvents.OnGameStarted -= HandleGameStarted;
+            GameEvents.OnGamePaused -= HandleGamePaused;
+            GameEvents.OnLevelCompleted -= HandleLevelCompleted;
+            GameEvents.OnLevelFailed -= HandleGameFailed;
+        }
+    }
+}
