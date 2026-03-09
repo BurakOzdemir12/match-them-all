34ac52b76e21623b5273eab2e5cf0a4506a3c0db
BurakOzdemir <qburak.ozdemir.0640@gmail.com>
2026-03-09 18:21:59 +0300

    feat(ui): Game Over Panel

---
+++ Assets/_Project/Scripts/UI/Components/GameOverPanelUI.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using _Project.Scripts.Enums;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Project.Scripts.UI.Components
+{
+    public class GameOverPanelUI : MonoBehaviour
+    {
+        public static event Action OnTryAgainClicked;
+        public static event Action OnLobbyClicked;
+        public static event Action<BoosterType> OnBoosterClicked;
+
+        public void TryAgainButtonClick()
+        {
+            OnTryAgainClicked?.Invoke();
+        }
+
+        public void LobbyButtonClick()
+        {
+            OnLobbyClicked?.Invoke();
+        }
+
+        public void BoosterButtonClick(BoosterType boosterType)
+        {
+            OnBoosterClicked?.Invoke(boosterType);
+        }
+    }
+}
