d02e164b55104de3ab15a1c6bc378a4dd7d0d3fd
BurakOzdemir <qburak.ozdemir.0640@gmail.com>
2026-03-14 20:36:05 +0300

    feat: Booster features management

---
+++ Assets/_Project/Scripts/Enums/ResourceType.cs
@@ -0,0 +1,10 @@
+﻿namespace _Project.Scripts.Enums
+{
+    public enum ResourceType
+    {
+        Coin,
+        Wrench,
+        FreezeTimeBooster,
+        LaserBooster
+    }
+}
---
+++ Assets/_Project/Scripts/Managers/BoosterManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Project.Scripts.Managers
+{
+    public class BoosterManager : MonoBehaviour
+    {
+        public static BoosterManager Instance { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(this.gameObject);
+                return;
+            }
+
+            Instance = this;
+        }
+    }
+}
