627691cd8daaa2572029f9015b4b56d242c7e543
BurakOzdemir <qburak.ozdemir.0640@gmail.com>
2026-03-18 13:31:33 +0300

    feat: Hammer booster animation complete

--- Assets/_Project/Scripts/Mechanics/Boosters/HammerBoosterMechanic.cs
+++ Assets/_Project/Scripts/Mechanics/Boosters/HammerBoosterMechanic.cs
@@ -19,7 +19,23 @@ namespace _Project.Scripts.Mechanics.Boosters
         [SerializeField] private float hammerScale = 2f;
 
         [Header("Hammer Animation Settings")] [Tooltip("Hammer one Hit animation Duration")] [SerializeField]
-        private float hammerHitDuration = 0.4f;
+        private float hammerMoveDuration = 0.4f;
+
+        [Tooltip("Hammer Move point vector offset")] [SerializeField]
+        private Vector3 hammerMovePosOffset;
+
+        [Tooltip("Hammer rotation duration to target")] [SerializeField]
+        private float hammerRotateDuration = 0.4f;
+
+        [Tooltip("Hammer Hit Rotation vector value")] [SerializeField]
+        private Vector3 hammerHitRotationVector = new Vector3(0, 0, -90);
+
+        [Tooltip("Target punch scale on hit")] [SerializeField]
+        private float targetPunchScale = 0.5f;
+
+        [Tooltip("Hammer Hit duration")] [SerializeField]
+        private float hammerHitDuration = 0.5f;
+
 
         private void Awake()
         {
@@ -41,32 +57,64 @@ namespace _Project.Scripts.Mechanics.Boosters
 
             GameEvents.TriggerBoosterAnimationStarted(ResourceType.HammerBooster);
 
-            GameObject hammer = Instantiate(hammerPrefab, pos ,
+            GameObject hammer = Instantiate(hammerPrefab, pos,
                 Quaternion.identity);
             hammer.transform.localScale = Vector3.one * hammerScale;
 
 
             foreach (var target in targets)
             {
-                var targetPos = target.transform.position;
+                Vector3 targetPos = target.transform.position;
+                Vector3 directionToTarget = (targetPos - hammer.transform.position).normalized;
+                directionToTarget.y = 0;
 
-                seq.Append(hammer.transform.DOMove(targetPos , hammerHitDuration)
-                    .SetEase(Ease.OutQuad)
-                );
-                seq.Append(target.transform.DOPunchScale(Vector3.one * 0.5f, 0.3f).SetEase(Ease.OutElastic));
+                Quaternion lookRotation = Quaternion.LookRotation(directionToTarget);
+                Vector3 targetEuler = lookRotation.eulerAngles;
 
-                GameEvents.TriggerBoosterUseRequested(ResourceType.HammerBooster, target);
+                targetEuler.y = -90f;
+
+                //TODO Sound effect for move 
+                
+                //? Hammer moves to the target pos with specified offset
+                seq.Append(hammer.transform.DOMove(targetPos + hammerMovePosOffset, hammerMoveDuration)
+                    .SetEase(Ease.OutQuad));
+
+                //? Hammer rotates to look at the target -> with euler.y = -90, because flat face must look to the object
+                seq.Join(hammer.transform.DORotate(targetEuler, hammerRotateDuration, RotateMode.Fast)
+                    .SetEase(Ease.OutQuad));
+
+                //? This is basically hammering action -> set Vector z = -90 its perfectly plays
+                seq.Append(hammer.transform
+                    .DORotate(hammerHitRotationVector, hammerHitDuration, RotateMode.LocalAxisAdd)
+                    .SetEase(Ease.InBack));
+                //TODO Sound effect for hammering
+
+                //? whenever hit animation complete play sound or decrease goal or something else
+                seq.AppendCallback(() =>
+                {
+                    GameEvents.TriggerBoosterUseRequested(ResourceType.HammerBooster, target);
+                });
+
+                //? This is the hit Target object explosion effect
+                seq.Append(target.transform.DOPunchScale(Vector3.one * targetPunchScale, 0.2f)
+                    .SetEase(Ease.OutElastic));
+                //TODO target Explosion sound effect
+                //? Just before the returning back to the spawn pos move a little bit higher
+                seq.Append(hammer.transform.DOMoveY(targetPos.y + 1f, 0f));
             }
 
             seq.OnComplete(() =>
             {
+                //? Animation ends then publish event icon must listen for fade in fade out transactions.
                 GameEvents.TriggerBoosterAnimationEnded(ResourceType.HammerBooster);
 
+                //? Target items will destroy
                 ItemSpotsManager.Instance.DestroyItemsFromBoard(targets);
 
+                //? Hammer Return back to the first spawned position.
                 Sequence finishSeq = DOTween.Sequence().SetLink(this.gameObject);
 
-                finishSeq.Join(hammer.transform.DOMove(pos, hammerHitDuration)
+                finishSeq.Join(hammer.transform.DOMove(pos, hammerMoveDuration)
                     .SetEase(Ease.OutQuad));
 
                 finishSeq.OnComplete(() => { Destroy(hammer); });
