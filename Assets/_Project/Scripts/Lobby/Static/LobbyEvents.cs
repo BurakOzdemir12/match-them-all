b6f15597cb3b6bc8f6cd76b6b7ee9251db553cea
BurakOzdemir <qburak.ozdemir.0640@gmail.com>
2026-04-08 15:51:13 +0300

    feat: LobbyEvents enum for control lobby events in one place

---
+++ Assets/_Project/Scripts/Lobby/Static/LobbyEvents.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using _Project.Scripts.Lobby.Data.Save;
+using _Project.Scripts.Lobby.ScriptableObjects.Plane;
+using _Project.Scripts.Lobby.Structs;
+
+namespace _Project.Scripts.Lobby.Static
+{
+    public struct LobbyEvents
+    {
+        public static event Action<PlanePartSo, PartSaveInfo> OnPlanePartLoaded;
+        public static event Action<PlanePartSo, PlanePartVariation> OnPlanePartBuildStarted;
+        public static event Action<PlanePartSo> OnPlanePartBuildTargeted;
+        public static event Action<PlaneBluePrintSo> OnPlanePartBuildCompleted;
+
+        public static void TriggerPlanePartLoaded(PlanePartSo part, PartSaveInfo saveInfo)
+        {
+            OnPlanePartLoaded?.Invoke(part, saveInfo);
+        }
+
+        public static void TriggerPlanePartBuildStarted(PlanePartSo partSo, PlanePartVariation selectedVariation)
+        {
+            OnPlanePartBuildStarted?.Invoke(partSo, selectedVariation);
+        }
+
+        public static void TriggerNextPartTargeted(PlanePartSo nextPart)
+        {
+            OnPlanePartBuildTargeted?.Invoke(nextPart);
+        }
+
+        public static void TriggerPlaneBuildCompleted(PlaneBluePrintSo planeBluePrintSo)
+        {
+            OnPlanePartBuildCompleted?.Invoke(planeBluePrintSo);
+        }
+    }
+}
