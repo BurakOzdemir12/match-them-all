501ada166bfbfbbb0c8c0ef94b61e0791f9a4f3e
BurakOzdemir <qburak.ozdemir.0640@gmail.com>
2026-04-09 13:03:46 +0300

    feat: Create plane build UI workflow.

---
+++ Assets/_Project/Scripts/Lobby/UI/Components/PlaneBuildChoiceCardUI.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using _Project.Scripts.Lobby.ScriptableObjects.Plane;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Project.Scripts.Lobby.UI.Components
+{
+    public class PlaneBuildChoiceCardUI : MonoBehaviour
+    {
+        [Header("UI References")] [Tooltip("How many wrench require to build this part")] [SerializeField]
+        private TextMeshProUGUI wrenchRequire;
+
+        [Tooltip("Build Part Icon")] [SerializeField]
+        private Image partIcon;
+
+        [Tooltip("Description text for build")] [SerializeField]
+        private TextMeshProUGUI descriptionText;
+
+        private PlanePartSo _currentPartSo;
+
+        public event Action<PlanePartSo> OnBuildChoiceSelected;
+
+        public void Setup(PlanePartSo partSo)
+        {
+            _currentPartSo = partSo;
+            wrenchRequire.text = partSo.requiredWrench.ToString();
+            descriptionText.text = partSo.descriptionText;
+            partIcon.sprite = partSo.partIcon;
+        }
+
+        public void OnBuildButtonClick()
+        {
+            OnBuildChoiceSelected?.Invoke(_currentPartSo);
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Lobby/UI/Managers/PlaneBuildUIManager.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using _Project.Scripts.Lobby.Enums;
+using _Project.Scripts.Lobby.Managers;
+using _Project.Scripts.Lobby.ScriptableObjects.Plane;
+using _Project.Scripts.Lobby.Static;
+using _Project.Scripts.Lobby.Structs;
+using _Project.Scripts.Lobby.UI.Components;
+using UnityEngine;
+
+namespace _Project.Scripts.Lobby.UI.Managers
+{
+    public class PlaneBuildUIManager : MonoBehaviour
+    {
+        [Header("UI References")] [SerializeField]
+        private Transform gridContainer;
+
+        [SerializeField] private GameObject buildChoiceCardPrefab;
+
+        private readonly Dictionary<PlanePartSo, PlaneBuildChoiceCardUI> spawnedChoices =
+            new Dictionary<PlanePartSo, PlaneBuildChoiceCardUI>();
+
+        private void OnEnable()
+        {
+            LobbyEvents.OnPlanePartBuildTargeted += HandlePlanePartBuildTargeted;
+            LobbyEvents.OnPlanePartBuildStarted += HandlePlaneBuildStarted;
+        }
+
+        private void HandlePlaneBuildStarted(PlanePartSo partSo, PlanePartVariation partVariation)
+        {
+            if (spawnedChoices.TryGetValue(partSo, out PlaneBuildChoiceCardUI card))
+            {
+                card.OnBuildChoiceSelected -= HandleBuildChoiceSelected;
+                Destroy(card.gameObject);
+                spawnedChoices.Remove(partSo);
+            }
+        }
+
+        private void HandlePlanePartBuildTargeted(PlanePartSo partSo)
+        {
+            InitializeBuildChoiceCards(partSo);
+        }
+
+        private void InitializeBuildChoiceCards(PlanePartSo partSo)
+        {
+            ClearAllCards();
+
+            PlaneBuildChoiceCardUI newCard =
+                Instantiate(buildChoiceCardPrefab, gridContainer).GetComponent<PlaneBuildChoiceCardUI>();
+
+            newCard.Setup(partSo);
+            newCard.OnBuildChoiceSelected += HandleBuildChoiceSelected;
+
+            spawnedChoices.Add(partSo, newCard);
+        }
+
+        private void HandleBuildChoiceSelected(PlanePartSo partSo)
+        {
+            PlaneBuildManager.Instance.TryBuildPart(partSo, partSo.variations[0]);
+        }
+
+        private void ClearAllCards()
+        {
+            foreach (var kvp in spawnedChoices)
+            {
+                if (kvp.Value != null)
+                {
+                    kvp.Value.OnBuildChoiceSelected -= HandleBuildChoiceSelected;
+                    Destroy(kvp.Value.gameObject);
+                }
+            }
+
+            spawnedChoices.Clear();
+        }
+
+        private void OnDisable()
+        {
+            LobbyEvents.OnPlanePartBuildTargeted -= HandlePlanePartBuildTargeted;
+            LobbyEvents.OnPlanePartBuildStarted -= HandlePlaneBuildStarted;
+
+            ClearAllCards();
+        }
+    }
+}
