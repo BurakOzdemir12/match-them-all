e41a5fe9c84c4d87fe9aac9d95f228baa31c29fc
BurakOzdemir <qburak.ozdemir.0640@gmail.com>
2026-04-11 12:51:12 +0300

    feat: Create socket manager and socket script for define aircraft part's position.

---
+++ Assets/_Project/Scripts/Lobby/Components/PlaneSocket.cs
@@ -0,0 +1,11 @@
+﻿using _Project.Scripts.Lobby.Enums;
+using UnityEngine;
+
+namespace _Project.Scripts.Lobby.Components
+{
+    public class PlaneSocket : MonoBehaviour
+    {
+        [field: Header("Plane part Type"), SerializeField]
+        public PlanePartType planePartType { get; private set; }
+    }
+}
---
+++ Assets/_Project/Scripts/Lobby/Managers/PlaneSocketManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using _Project.Scripts.Lobby.Components;
+using _Project.Scripts.Lobby.Enums;
+using _Project.Scripts.Lobby.Static;
+using UnityEngine;
+
+namespace _Project.Scripts.Lobby.Managers
+{
+    public class PlaneSocketManager : MonoBehaviour
+    {
+        public readonly Dictionary<PlanePartType, Transform> sockets = new Dictionary<PlanePartType, Transform>();
+
+
+        private void Awake()
+        {
+            PlaneSocket[] socketComponents = GetComponentsInChildren<PlaneSocket>();
+            foreach (PlaneSocket socket in socketComponents)
+            {
+                if (!sockets.ContainsKey(socket.planePartType))
+                {
+                    sockets.Add(socket.planePartType, socket.transform);
+                }
+                else
+                {
+                    Debug.LogWarning($"Duplicate socket found: {socket.planePartType}");
+                }
+            }
+        }
+
+        private void Start()
+        {
+            LobbyEvents.TriggerPlaneSpawned(this);
+        }
+    }
+}
