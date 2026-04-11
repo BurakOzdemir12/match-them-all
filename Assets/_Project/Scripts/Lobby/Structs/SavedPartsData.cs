469fbac18288ac0f950334ab89ccdd493e0ed5a7
BurakOzdemir <qburak.ozdemir.0640@gmail.com>
2026-04-11 15:19:32 +0300

    feat:  Save partSo and  part save info sin a separate struct.

---
+++ Assets/_Project/Scripts/Lobby/Structs/SavedPartsData.cs
@@ -0,0 +1,18 @@
+﻿using _Project.Scripts.Lobby.Data.Save;
+using _Project.Scripts.Lobby.ScriptableObjects.Plane;
+
+namespace _Project.Scripts.Lobby.Structs
+{
+    [System.Serializable]
+    public struct SavedPartData
+    {
+        public PlanePartSo partSo;
+        public PartSaveInfo saveInfo;
+
+        public SavedPartData(PlanePartSo partSo, PartSaveInfo saveInfo)
+        {
+            this.partSo = partSo;
+            this.saveInfo = saveInfo;
+        }
+    }
+}
