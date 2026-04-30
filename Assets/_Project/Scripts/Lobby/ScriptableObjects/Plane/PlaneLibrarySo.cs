9bd2de467bd0b4cdd00c7254c685f798cd519143
BurakOzdemir <qburak.ozdemir.0640@gmail.com>
2026-04-30 17:35:54 +0300

    feat: Scriptable object for hold aircrafts list

---
+++ Assets/_Project/Scripts/Lobby/ScriptableObjects/Plane/PlaneLibrarySo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace _Project.Scripts.Lobby.ScriptableObjects.Plane
+{
+    [CreateAssetMenu(fileName = "PlaneLibrary", menuName = "ScriptableObjects/Lobby/Plane/Plane Library")]
+    public class PlaneLibrarySo : ScriptableObject
+    {
+        [field: Header("All Planes In Game"), SerializeField]
+        public List<PlaneBluePrintSo> planeListInGame { get; private set; } = new List<PlaneBluePrintSo>();
+    }
+}
